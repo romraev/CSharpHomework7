ef89ef3ffc57fb3302e840a6eb1c66110a1a4351
Romraev <romraev@yandex.ru>
2023-03-30 18:29:45 +0300

    third task

--- Program.cs
+++ Program.cs
@@ -91,3 +91,64 @@ Find(myArray, row, col);
 // Task 52. Задайте двумерный массив из целых чисел. 
 // Найдите среднее арифметическое элементов в каждом столбце.
 
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Введите число строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число столбцов: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите минимальное число: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите максимальное число: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+    int[,] array = new int[rows, columns];
+    
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i,j] = new Random().Next(min, max + 1);
+    
+    return array;
+}
+
+void Write2dArray(int[,]array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[] MedNum(int[,] array)
+{
+    double[] array1 = new double[array.GetLength(1)];
+    double sum = 0;
+    
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int row = 0; row < array.GetLength(0); row++) 
+        {
+            sum += array[row,i];
+        }        
+        array1[i] = sum / array.GetLength(0); 
+        sum = 0;     
+    }
+    return array1;
+}
+
+void Write1dArray(double[]array)
+{
+    for(int i=0; i<array.Length; i++)
+        Console.Write(array[i] + "; ");
+
+    Console.WriteLine();
+}
+
+int[,] myArray = CreateRandom2dArray();
+Write2dArray(myArray);
+double[] array1d = MedNum(myArray);
+Console.Write("Среднее арифметическое значений в столбцах равно: ");
+Write1dArray(array1d);
