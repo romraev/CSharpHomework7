457e160461979ee587e31f1695307aad5b68c371
Romraev <romraev@yandex.ru>
2023-03-30 17:19:01 +0300

    second task

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 // Task 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-double[,] CreateRandom2dArray()
+/* double[,] CreateRandom2dArray()
 {
     Console.Write("Введите число строк: ");
     int rows = Convert.ToInt32(Console.ReadLine());
@@ -35,3 +35,59 @@ void WriteArray(double[,]array)
 
 double[,] myArray = CreateRandom2dArray();
 WriteArray(myArray);
+*/
+
+// Task 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+/* int[,] CreateRandom2dArray()
+{
+    Console.Write("Введите число строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число столбцов: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите минимальное число: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите максимальное число: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+    int[,] array = new int[rows, columns];
+    
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i,j] = new Random().Next(min, max + 1);
+    
+    return array;
+}
+
+void WriteArray(int[,]array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void Find(int[,] array, int r, int c)
+{
+    if(r > array.GetLength(0) || c > array.GetLength(1)) Console.Write("Такого значения нет");
+    else Console.WriteLine($"Значение с позицией [{r}, {c}] - {array[r-1,c-1]}");
+}
+
+int[,] myArray = CreateRandom2dArray();
+WriteArray(myArray);
+
+Console.Write("Введите номер строки: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int col = Convert.ToInt32(Console.ReadLine());
+
+Find(myArray, row, col);
+*/
+
+// Task 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
